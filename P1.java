1d2fe325a8b9462b77bb9757e0873ce979d56599
SAMTECH-KO <samnitkorvi@gmail.com>
2025-07-13 19:55:44 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,41 @@
+/*
+ 	* * * * *
+ 	*		*
+ 	* 		*
+ 	* 		*
+ 	* 		*
+ 	* * * * *
+ */
+
+public class P1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 1; i <= 6; i++) {
+			
+			if(i == 1 || i == 6) {
+				for(int j=1; j < 6; j++) {
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+			
+			else {
+				
+				for(int j = 1; j < 6; j++ ) {
+					if(j == 2 || j == 3 || j == 4) {
+						System.out.print("  ");
+					}
+					
+					else {
+						System.out.print("* ");
+					}
+				
+				}
+				
+				System.out.println();
+					
+			} 
+			} 
+		} } 
