fa4a06b3b33d770296cb7efaed65d11849ac5cd8
SAMTECH-KO <samnitkorvi@gmail.com>
2025-07-13 22:48:04 +0530

    Add files via upload

---
+++ P7.java
@@ -0,0 +1,23 @@
+/*
+
+4 3 2 1
+3 2 1
+2 1 
+1
+
+*/
+
+public class P7 {
+
+	public static void main(String[] args){
+
+		int n = 4;
+		for(int i = n; i >= 1; i--) {
+			for(int j = i; j >= 1; j--) {
+				System.out.print(j + " ");
+			}
+
+			System.out.println();
+		}
+
+} }
